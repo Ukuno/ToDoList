4407131194c6f58479b318c06e5048cc8d49fde5
ukuno <yukunos123@gmail.com>
2017-12-24 11:47:02 -0800

    first commit to upload the app

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+//check off selected todo by checking
+$("ul").on('click','li', function(){
+	$(this).toggleClass('completed');
+});
+
+//click to delete list
+
+$("ul").on('click','span',function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+	// propagation stops the event bubbling to the parent event
+});
+
+$("input[type='text']").on('keypress',function(event){
+	if(event.which === 13){
+		// grabs the new todo list 
+		var addToDoList = $(this).val();
+		// clears the input text 
+		$(this).val("");
+		//add to the list of todo list
+
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+addToDoList+"</li>");
+
+	}
+});
+
+
+
+$('.fa-bars').click(function(){
+	$("input[type='text']").fadeToggle();
+});
